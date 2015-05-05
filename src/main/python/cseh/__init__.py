ada5656cedf360544ccc91813f115a8df52ef930
Konrad Hosemann <konrad@hosemann.name>
2015-05-05 13:54:58 +0200

    unit test stub

--- src/main/python/cseh/__init__.py
+++ src/main/python/cseh/__init__.py
@@ -1,7 +1,16 @@
 from boto.utils import get_instance_metadata
 
+
+def _get_instance_id():
+    return get_instance_metadata()['instance-id']
+
+
+def _get_region():
+    return get_instance_metadata()['availability-zone'][:-1]
+
+
 def signal_elb_healthcheck(elb_name):
-    get_instance_metadata()
+    instance_id = _get_instance_id()
     pass
 
 
---
+++ src/test/python/cseh/test_signal_elb_healthcheck.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestSignal_elb_healthcheck(TestCase):
+  def test_get_region_strips_availability_zone_to_region(self):
+    pass
