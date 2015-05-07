600197b93b05b22b880c604bc0f2c2e88bb205ba
Konrad Hosemann <konrad@hosemann.name>
2015-05-07 11:56:25 +0200

    show stacktrace and fix metadata access

--- src/main/python/cseh/__init__.py
+++ src/main/python/cseh/__init__.py
@@ -5,9 +5,10 @@ from boto.utils import get_instance_metadata
 
 logger = logging.getLogger(__name__)
 
-def _get_instance_id():
-    return get_instance_metadata()['instance-id']
+
+def get_instance_id():
+    return get_instance_metadata().get('instance-id')
 
 
 def get_region():
-    return get_instance_metadata()['availability-zone'][:-1]
+    return get_instance_metadata().get('placement')['availability-zone'][:-1]
