786255fdb4654798c031237bb543fabe36b93e58
Konrad Hosemann <konrad@hosemann.name>
2015-05-27 15:27:53 +0200

    switch to botocore, which implements retry logic

--- src/main/python/cseh/HealthChecker.py
+++ src/main/python/cseh/HealthChecker.py
@@ -1,7 +1,7 @@
 import logging
 from time import sleep
 
-from boto.ec2.elb import connect_to_region
+import botocore.session
 from cseh import get_region, get_instance_id
 from cseh.Signaller import Signaller
 
@@ -14,14 +14,14 @@ class HealthChecker(object):
         self.logical_resource_id = logical_resource_id
         self.region = region or get_region()
         self.instance_id = instance_id or get_instance_id()
-        self.elb_conn = connect_to_region(self.region)
+        self.elb_client = botocore.session.get_session().create_client("elb", region_name=self.region)
         self.logger = logging.getLogger(__name__)
         self.signaller = Signaller(instance_id, region)
 
     def elb_healthcheck(self, elb_name):
         self.logger.info("trying describe instance health at ELB API")
-        instance_state = self.elb_conn.describe_instance_health(elb_name, [self.instance_id])
-        state = instance_state[0].state
+        result = self.elb_client.describe_instance_health(LoadBalancerName=elb_name, Instances=[{"InstanceId": self.instance_id}])
+        state = result["InstanceStates"][0]["State"]
         self.logger.info("ELB state for instance {0}: {1}".format(self.instance_id, state))
         return state == 'InService'
 
